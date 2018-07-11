cf4b39d93ad88acc5dff72010b4a18368968ed72
dave <dave@computesoftware.com>
2018-07-11 14:30:41 -0700

    Remove debug

--- src/lein_tools_deps/deps.clj
+++ src/lein_tools_deps/deps.clj
@@ -3,7 +3,14 @@
             [lein-tools-deps.file-attributes :as file-attributes]
             [lein-tools-deps.env :as env]
             [clojure.tools.deps.alpha.reader :as reader]
-            [clojure.string :as s]))
+            [clojure.string :as s]
+            [clojure.pprint :as pp]))
+
+(defn- replace-drive-path
+  [p [drive-regex drive-path]]
+  (if drive-regex
+    (s/replace-first p drive-regex drive-path)
+    p))
 
 (defn make-dep-loc-lookup
   "Returns a function mapping from a loc(ation)
@@ -11,20 +18,14 @@
   location.  If the value is a string it is returned as is."
   [{:keys [config-files]} path-replacement]
   (let [[system-deps home-deps project-deps] config-files
-        project-deps (or project-deps "deps.edn")
-        os (.toLowerCase (System/getProperty "os.name"))
-        [_ [drive-regex drive-path]] (->> path-replacement
-                                          (filter (fn [[k v]] (some? (s/index-of os k))))
-                                          first)]
+        project-deps (or project-deps "deps.edn")]
     (fn [i]
       (let [p (if (string? i)
                 i
                 ({:install system-deps
                   :user home-deps
                   :project project-deps} i))]
-        (if drive-regex
-          (s/replace-first p drive-regex drive-path)
-          p)))))
+        (replace-drive-path p path-replacement)))))
 
 
 (defn canonicalise-dep-locs
@@ -40,59 +41,65 @@
 (defn absolute-local-root-coords
   "Given a base path and :local/root coordinates, ensures the specified path
   is absolute relative to the base path."
-  [{:keys [local/root]} base-path]
-  {:local/root (file-attributes/absolute-path base-path root)})
+  [{:keys [local/root]} base-path path-replacement]
+  {:local/root (file-attributes/absolute-path (replace-drive-path base-path path-replacement) (replace-drive-path root path-replacement))})
 
 (defn absolute-coords
   "Given a base path and dep coordinates, ensures any paths in the coordinates
   are absolute relative to the base path."
-  [coords base-path]
+  [coords base-path path-replacement]
   (if (contains? coords :local/root)
-    (absolute-local-root-coords coords base-path)
+    (absolute-local-root-coords coords base-path path-replacement)
     coords))
 
 (defn absolute-deps-map
   "Given a base path and a deps map (a mapping from lib symbol to
   coordinates), ensures that any relative paths embedded in coordinates are
   absolute relative to the base path."
-  [deps-map base-path]
+  [deps-map base-path path-replacement]
   (->> deps-map
        (map (fn [[dep coords]]
               [dep (if (string? coords)
-                     (file-attributes/absolute-path base-path coords)
-                     (absolute-coords coords base-path))]))
+                     (file-attributes/absolute-path (replace-drive-path base-path path-replacement) (replace-drive-path coords path-replacement))
+                     (absolute-coords coords base-path path-replacement))]))
        (into {})))
 
 (defn- alias-absolute-deps-map
   "Given a base path and a map of aliases, ensure that any relative paths
   embedded in the extra-dep or classpath-overrides keys are absolute
   relative to the base path."
-  [aliases base-path]
+  [aliases base-path path-replacement]
   (->> aliases
        (map (fn [[alias info]]
               [alias (->> [:extra-deps :classpath-overrides]
                           (select-keys info)
                           keys
-                          (reduce (fn [acc k] (update acc k absolute-deps-map base-path)) info))]))
+                          (reduce (fn [acc k] (update acc k absolute-deps-map base-path path-replacement)) info))]))
        (into {})))
 
 (defn absolute-deps
   "Given a base path and deps, ensures that all absolute paths in the deps
   (including relative deps embedded in aliases) are absolute relative to the
   base path."
-  [deps base-path]
+  [deps base-path path-replacement]
   (-> deps
-      (update :deps absolute-deps-map base-path)
-      (update :aliases alias-absolute-deps-map base-path)))
+      (update :deps absolute-deps-map base-path path-replacement)
+      (update :aliases alias-absolute-deps-map base-path path-replacement)))
 
 (defn make-deps
   "Reads and merges all of the deps-ref, returning a single deps map"
   ([exists? read-deps env {:keys [root] {:keys [config-files path-replacement]} :lein-tools-deps/config}]
-   (as-> config-files $
-         (canonicalise-dep-locs env root $ path-replacement)
-         (filter exists? $)
-         (read-deps $)
-         (absolute-deps $ root)))
+   (let [os (.toLowerCase (System/getProperty "os.name"))
+         [_ path-replacement] (->> path-replacement
+                                   (filter (fn [[k v]] (some? (s/index-of os k))))
+                                   first)
+         deps (as-> config-files $
+                    (canonicalise-dep-locs env root $ path-replacement)
+                    (filter exists? $)
+                    (read-deps $)
+                    (absolute-deps $ root path-replacement))]
+     #_(clojure.pprint/pprint deps)
+     deps))
   ([env project]
    (make-deps env/exists? reader/read-deps env project)))
 
